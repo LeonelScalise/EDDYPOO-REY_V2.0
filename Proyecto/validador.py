95cdeb7f7e3a44043e30c0190bba522f04c6fbb6
telffman <101122900+telffman@users.noreply.github.com>
2023-05-30 19:10:05 -0300

    Update validador.py

--- Proyecto/validador.py
+++ Proyecto/validador.py
@@ -195,3 +195,19 @@ def validadorNota():
                 print(e)
     
     return Nota_final
+
+def validadorSexo():
+    inicio = True
+    while inicio:
+        try:
+            sexo = input("\nIngrese el sexo (M/F): ")
+            if sexo not in ["M", "F"]:
+                raise Exception("\nEl sexo debe ser 'M' o 'F'.\n")
+            else:
+                inicio = False
+        except ValueError:
+                print('\nEl dato introducido no corresponde al valor esperado.\n')
+        except Exception as e: 
+                print(e)
+    
+    return sexo
