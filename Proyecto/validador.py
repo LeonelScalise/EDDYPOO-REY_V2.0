814a08700b4b2a5905163641f93aa6473c50e9b8
telffman <101122900+telffman@users.noreply.github.com>
2023-05-30 18:30:37 -0300

    Update validador.py

--- Proyecto/validador.py
+++ Proyecto/validador.py
@@ -1,5 +1,6 @@
 import re
 from popularInstitucion import *
+from datetime import *
 
 def validadorAula():
     inicio = True
@@ -55,3 +56,20 @@ def validadorDia():
                 print(e)
     
     return dias
+
+def validadorFecha():
+    inicio = True
+    while inicio:
+        try:
+            fecha = input(f'\nIngrese la fecha de nacimiento: ') 
+            if datetime.strptime(fecha, '%d/%m/%Y'):
+                if int(datetime.today().year) - int(datetime.strptime(fecha, '%d/%m/%Y').year) >= 18:
+                    inicio = False
+                else:
+                    raise Exception("\nLa persona tiene que ser mayor de edad\n")
+        except ValueError:
+                print('\nIntroduzca una fecha con formato valido (dd/mm/yyyy)\n')
+        except Exception as e: 
+                print(e)
+    
+    return datetime.strptime(fecha, '%d/%m/%Y')
