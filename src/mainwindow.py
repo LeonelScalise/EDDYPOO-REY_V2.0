df1034a3e7e870961aa24a9cd577c228fe14b546
LeonelScalise <lscalise@itba.edu.ar>
2023-06-17 18:50:27 -0300

    ui de login

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -2,6 +2,7 @@
 from math import inf
 from PyQt5.QtWidgets import QMainWindow, QListWidgetItem, QColorDialog, QFileDialog, QStyle
 from PyQt5.QtCore import Qt, QCoreApplication
+from PyQt5 import uic
 
 # Project modules
 from src.ui.mainwindow import Ui_MainWindow
@@ -12,5 +13,3 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-
-        self.btn_admin.clicked.connect()
