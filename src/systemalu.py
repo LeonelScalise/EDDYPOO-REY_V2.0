475e0e9dea709ea73f1a05b567637b29c3a59777
LeonelScalise <lscalise@itba.edu.ar>
2023-06-21 01:36:29 -0300

    Algunos ajustes en los diseños + conexion btn-menu

--- src/systemalu.py
+++ src/systemalu.py
@@ -10,3 +10,26 @@ class SystemaluWindow(QMainWindow, Ui_Systemalu):
     def __init__(self):
         super(SystemaluWindow, self).__init__()
         self.setupUi(self)
+
+        # Configuración botones del menu
+        self.btn_inscr_mat_alu.clicked.connect(self.showWindowInscribirMateria)
+        self.btn_desinscr_mat_alu.clicked.connect(self.showWindowDesinscribirMateria)
+        self.btn_iniciar_tramite_alu.clicked.connect(self.showWindowIniciarTramite)
+        self.btn_modif_pass_alu.clicked.connect(self.showWindowCambiarContrasena)
+        self.btn_stats_academicas.clicked.connect(self.showWindowEstadisticasAcademicas)
+        
+    # Funciones que activan los botones para cambiar los menus
+    def showWindowInscribirMateria(self):
+        self.stackedWidget.setCurrentWidget(self.page_inscr_mat)
+
+    def showWindowDesinscribirMateria(self):
+        self.stackedWidget.setCurrentWidget(self.page_desinscr_mat)
+
+    def showWindowIniciarTramite(self):
+        self.stackedWidget.setCurrentWidget(self.page_iniciar_tramite_alu)
+
+    def showWindowCambiarContrasena(self):
+        self.stackedWidget.setCurrentWidget(self.page_modif_pass_alu)
+
+    def showWindowEstadisticasAcademicas(self):
+        self.stackedWidget.setCurrentWidget(self.page_stats_academicas)
--- src/systemprofe.py
+++ src/systemprofe.py
@@ -10,3 +10,19 @@ class SystemprofeWindow(QMainWindow, Ui_Systemprofe):
     def __init__(self):
         super(SystemprofeWindow, self).__init__()
         self.setupUi(self)
+        
+        
+        # Configuración botones del menu
+        self.btn_subir_nota_final.clicked.connect(self.showWindowSubirNotaFinal)
+        self.btn_modif_pass_profe.clicked.connect(self.showWindowCambiarContrasena)
+        self.btn_iniciar_tramite_profe.clicked.connect(self.showWindowIniciarTramite)
+
+    # Funciones que activan los botones para cambiar los menus
+    def showWindowSubirNotaFinal(self):
+        self.stackedWidget.setCurrentWidget(self.page_subir_nota)
+
+    def showWindowCambiarContrasena(self):
+        self.stackedWidget.setCurrentWidget(self.page_modif_pass_profe)
+
+    def showWindowIniciarTramite(self):
+        self.stackedWidget.setCurrentWidget(self.page_iniciar_tramite_profe)
